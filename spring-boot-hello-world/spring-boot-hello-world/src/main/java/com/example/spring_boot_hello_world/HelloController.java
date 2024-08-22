66f4a9f241e7dff0372d358210fd6d4f63ecb2c9
abhi147p <abhishekpuppala60@gmail.com>
2024-08-22 17:50:52 -0500

    Spring boot application

---
+++ spring-boot-hello-world/spring-boot-hello-world/src/main/java/com/example/spring_boot_hello_world/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_hello_world;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/home")
+    public String helloworld(){
+        return "Hello World!";
+    }
+}
