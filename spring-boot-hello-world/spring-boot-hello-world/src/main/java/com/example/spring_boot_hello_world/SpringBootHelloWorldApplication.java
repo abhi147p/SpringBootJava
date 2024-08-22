4b099ffe9b3cf8338e1117723f3597db12e4f213
abhi147p <abhishekpuppala60@gmail.com>
2024-08-22 18:04:20 -0500

    Command Line Runner CH 58

--- spring-boot-hello-world/spring-boot-hello-world/src/main/java/com/example/spring_boot_hello_world/SpringBootHelloWorldApplication.java
+++ spring-boot-hello-world/spring-boot-hello-world/src/main/java/com/example/spring_boot_hello_world/SpringBootHelloWorldApplication.java
@@ -1,14 +1,23 @@
 package com.example.spring_boot_hello_world;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class SpringBootHelloWorldApplication {
+public class SpringBootHelloWorldApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(SpringBootHelloWorldApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(" *** Show Arguments *** ");
+		Arrays.stream(args).forEach(System.out::println);
+		// For passing the args, we can do using the edit configurations.
+	}
 }
