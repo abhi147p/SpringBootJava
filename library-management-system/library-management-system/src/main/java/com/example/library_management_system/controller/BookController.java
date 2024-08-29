94fdf2184d49ab850f3d4f37b81d9811358b2d21
abhi147p <abhishekpuppala60@gmail.com>
2024-08-29 05:37:14 -0500

    Updated logout and display of edit and delete buttons according to user roles

--- library-management-system/library-management-system/src/main/java/com/example/library_management_system/controller/BookController.java
+++ library-management-system/library-management-system/src/main/java/com/example/library_management_system/controller/BookController.java
@@ -4,6 +4,8 @@ package com.example.library_management_system.controller;
 import com.example.library_management_system.entity.Book;
 import com.example.library_management_system.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,9 +25,17 @@ public class BookController {
 
     @GetMapping("/")
     public String getAllBooks(Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        System.out.println("Logged in user: " + auth.getName());
+        //model.addAttribute("username", auth.getName());
         List<Book> books = bookService.findAllBooks();
         model.addAttribute("books", books);
         return "index";
+    }
+
+    @PostMapping("/logout")
+    public String logoutPage(Model model){
+        return "/logout";
     }
 
     @PostMapping("/addBook")
